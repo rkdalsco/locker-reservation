c98cc373992829f8599be7a818c8221e6f6f701d
kimharry <kimharry0610@gmail.com>
2025-03-18 18:02:37 +0900

    tlqkf

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+    SupabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdm94cWpsZGpueWJjcHhwb3ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMDI0NjIsImV4cCI6MjA1Nzc3ODQ2Mn0.6OIV_5FZFg_M-9W0YjIXCbBKzjvORujzhulbUo3zG8g',
+    SupabaseUrl: 'https://djvoxqjldjnybcpxpovq.supabase.co'
+}
